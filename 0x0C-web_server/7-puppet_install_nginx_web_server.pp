32e092d79b5c678bc441391a4471ee788732ee20
ONGORO <jumaongoro5@gmail.com>
2023-12-13 07:41:13 -0800

    0-custom_http_response_header

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,21 @@
-# Configure a Nginx server with puppet
+# Automating project requirements using Puppet
 
-exec { 'nginx install':
-path    => '/usr/bin',
-command => 'sudo apt update && sudo apt-get -y install nginx',
+package { 'nginx':
+  ensure => installed,
 }
 
-# exec { 'Make file':
-# path    => '/usr/bin',
-# command => 'echo "Hello World!" > /var/www/html/index.html'
-# }
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/Codejuma permanent;',
+}
 
 file { '/var/www/html/index.html':
-mode    => '0755',
-content => 'Hello World!',
+  content => 'Hello World!',
 }
 
-exec { 'block server':
-path    => '/usr/bin',
-command => 'echo "
-server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
- 
-        location /redirect_me {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }
-}" | sudo tee /etc/nginx/sites-available/default'
-}
-
-exec { 'Restart nginx':
-path    => '/usr/bin',
-command => 'sudo service nginx restart',
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
